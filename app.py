30cca2609d0b424d5f5b0e543f2d76ccebe19ef5
pranav12060 <65703024+pranav12060@users.noreply.github.com>
2021-12-19 12:49:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+// insect classication app by Pranav Shinde.
+import streamlit as st
+def main():
+    file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
+    class_btn = st.button("Classify")
+    if file_uploaded is not None:    
+        image = Image.open(file_uploaded)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+
+
+
+if __name__ == "__main__":
+    main()
+
