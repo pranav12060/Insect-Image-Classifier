594b0009391021ad8111473426e7cee0f57f5c83
pranav12060 <65703024+pranav12060@users.noreply.github.com>
2022-01-15 21:37:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,27 @@
 import streamlit as st
 from PIL import Image
 def ain():
+ 
  if file_uploaded is not None:    
         my_images = Image.open(file_uploaded)
         st.image(my_images, caption='Uploaded Image', use_column_width=True)
         my_images.show()
    
-    for i, image_name in enumerate(my_images):  
-        if (image_name.split('.')[1] == 'jpg'):        
-            image = io.imread(image_directory + image_name)        
-            image = Image.fromarray(image, 'RGB')        
-            image = image.resize((SIZE,SIZE)) 
-            dataset.append(np.array(image))
-        x = np.array(dataset)
-    str=st.text_input(" Enter the directory name in which file to be stored \n ")
-    i = 0
-    for batch in datagen.flow(x, batch_size=16,save_to_dir= r'str',save_prefix='dr',save_format='jpg'):
-        i += 1
-        if i > 10:
-            break 
+ for i, image_name in enumerate(my_images):
+  if (image_name.split('.')[1] == 'jpg'):
+   image = io.imread(image_directory + image_name)        
+   image = Image.fromarray(image, 'RGB')        
+   image = image.resize((SIZE,SIZE)) 
+        dataset.append(np.array(image))
+   x = np.array(dataset)
+ str=st.text_input(" Enter the directory name in which file to be stored \n ")
+ i = 0
+ for batch in datagen.flow(x, batch_size=16,save_to_dir= r'str',save_prefix='dr',save_format='jpg'):
+  i += 1
+  if i > 10:
+   break 
+        
+     
 if __name__ == "__main__":
     file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
     class_btn = st.button("Create")
